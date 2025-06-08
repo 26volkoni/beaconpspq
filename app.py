030323edfe67cab45cd34d7a171eed4ab02dda2a
26volkoni <26volkoni@beaconschool.co.uk>
2025-06-08 19:37:09 +0000

    added image functionality

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
+import os
+import random
+import json
 
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    with open("locations.json") as f:
+        locations = json.load(f)
+    location= random.choice(locations)
+    image = location['image']
+    return render_template('index.html', image=image)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
